d2b21f79c1a0131234ae4bb6165cb00fd92fde0f
HariPrasath <34513494+Staunchers@users.noreply.github.com>
2020-02-07 11:17:00 +0530

    Rolling back Pug changes in master to generalize master.

--- Routes/shop.js
+++ Routes/shop.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 router.post("/Product", (request, response, next) => {
   //   console.log(app.get("books"));
   const data = request.body;
-  app.get("books").push( { Name:data.bName, Cost:data.bCost } );
+  app.get("books").push( { Name:data.pName, Cost:data.pCost } );
   response.redirect("/Home");
 });
 
