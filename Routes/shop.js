baf8d523b446319cabdfe9e02bdc0f4fd85474c3
Staunchers <34513494+Staunchers@users.noreply.github.com>
2020-02-08 14:26:15 +0530

    Till conditionals and loops in ejs

--- Routes/shop.js
+++ Routes/shop.js
@@ -2,7 +2,6 @@ const app = require("../app");
 const router = require("express").Router();
 
 router.post("/Product", (request, response, next) => {
-  //   console.log(app.get("books"));
   const data = request.body;
   app.get("books").push( { Name:data.pName, Cost:data.pCost } );
   response.redirect("/Home");
--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-app.engine("pug", require("pug").__express);
-app.set("view engine", "pug");
+// app.engine("pug", require("pug").__express);
+app.set("view engine", "ejs");
 app.set("views", "./Views");
 app.set("books", []);
 module.exports = app;
@@ -14,7 +14,8 @@ app.use(express.static("./Public"));
 
 //Home page with list of products
 app.use("/Home", (request, response, next) => {
- response.sendFile(path.join(__dirname, "./", "Views", "Home.html"));
+  const books = app.get('books');
+  response.render('Home',{books:books})
 });
 
 //Adding routes in external modules
@@ -25,7 +26,7 @@ app.use(require("./Routes/shop"));
 app.use((request, response, next) => {
   if (request.originalUrl != "/")  //wrong route
   {
-    response.status(404).sendFile(path.join(__dirname, "Views", "404.html"));
+    response.status(404).render('404',{page:404})
   }
   else //empty route
   { 
