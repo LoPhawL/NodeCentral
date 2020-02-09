f9f415cbb4ac23f65d8c6e5dbfa58b79211b921d
Staunchers <34513494+Staunchers@users.noreply.github.com>
2020-02-09 11:21:04 +0530

    navLink active

--- Routes/admin.js
+++ Routes/admin.js
@@ -3,7 +3,7 @@ const path = require("path");
 const dirname = require("../Utils/Path");
 
 router.use("/AddProduct", (request, response, next) => {
-  response.render('AddProduct', {page:'AddProduct'});
+  response.render('AddProduct', {page:'AddBook'});
 });
 
 module.exports = router;
