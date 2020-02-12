65a3cc5108ddc2245c6588670bb97334876aa2a7
HariPrasath <34513494+Staunchers@users.noreply.github.com>
2020-02-12 10:36:05 +0530

    Bug Fix

--- Modules/Admin/Controllers/ProductsController.js
+++ Modules/Admin/Controllers/ProductsController.js
@@ -1,6 +1,6 @@
 function Render_ProductsPage(response)
 {
-    response.render('Products_a', {module:'admin',page:'Products'});
+    response.render('Product_a', {module:'admin',page:'Products'});
 }
 
 module.exports = 
