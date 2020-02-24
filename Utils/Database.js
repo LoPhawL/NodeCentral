cff1763f498ad039fb844bc8f67ce7236c3bd232
Staunchers <34513494+Staunchers@users.noreply.github.com>
2020-02-24 22:15:32 +0530

    replaced database.js with sequelize

--- Utils/Database.js
+++ Utils/Database.js
@@ -1,24 +1,9 @@
-// const sql = require('mssql/msnodesqlv8');
-const sql = require('mssql');
-// const config ={
-//     database: "nodemane",
-//     server: "SONY-VAIO",
-//     driver: "msnodesqlv8",
-//     options: {
-//       trustedConnection: true
-//     }};
+const Sequelize = require('sequelize');
 
-    const config ={
-      database: "nodemane",
-      server: "nodemane.database.windows.net",
-      user: "nodemaneadmin",
-      password: "1234@Abcd",
-      options:
-      {
-        encrypt:true
-      }
-    };
-  
-// const connection = (new sql.ConnectionPool(config));
+const sequelize = new Sequelize('sequelize','nodemaneadmin','1234@Abcd',
+{
+  host:'nodemane.database.windows.net',
+  dialecT:'mssql'
+});
 
- module.exports =(new sql.ConnectionPool(config)).connect();// connection.connect();
+module.exports = sequelize;
