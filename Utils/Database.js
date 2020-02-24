d236dac3ca7bb128441f5d9e39b4a6a232bd5b95
Staunchers <34513494+Staunchers@users.noreply.github.com>
2020-02-24 19:49:35 +0530

    created and connected to azure nodemane db.

--- Utils/Database.js
+++ Utils/Database.js
@@ -1,16 +1,24 @@
-const sql = require('mssql/msnodesqlv8');
-// require("msnodesqlv8");
-const config ={
-    database: "UdmNode",
-    server: "SONY-VAIO",
-    driver: "msnodesqlv8",
-    options: {
-      trustedConnection: true
-    }};
+// const sql = require('mssql/msnodesqlv8');
+const sql = require('mssql');
+// const config ={
+//     database: "nodemane",
+//     server: "SONY-VAIO",
+//     driver: "msnodesqlv8",
+//     options: {
+//       trustedConnection: true
+//     }};
 
-const connection = (new sql.ConnectionPool(config));
-// .then(pool => {SetPool(pool); console.log(conPool);
-//});//.catch(err => console.log('error'));
-//  return pool;});
+    const config ={
+      database: "nodemane",
+      server: "nodemane.database.windows.net",
+      user: "nodemaneadmin",
+      password: "1234@Abcd",
+      options:
+      {
+        encrypt:true
+      }
+    };
+  
+// const connection = (new sql.ConnectionPool(config));
 
- module.exports = connection.connect();
+ module.exports =(new sql.ConnectionPool(config)).connect();// connection.connect();
