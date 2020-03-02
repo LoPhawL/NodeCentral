877b1396f796e6c4873ad962e398902e3204a0f8
Staunchers <34513494+Staunchers@users.noreply.github.com>
2020-03-02 20:25:48 +0530

    Deleted cart model.

--- Modules/EndUser/Controllers/OrdersController.js
+++ Modules/EndUser/Controllers/OrdersController.js
@@ -4,7 +4,6 @@ function Render_OrdersPage(response)
 {
     // response.render('Orders',{module:'enduser', page:'Orders'});
     Order.find().then(res => {response.send(res)});
-    
 }
 
 module.exports = 
